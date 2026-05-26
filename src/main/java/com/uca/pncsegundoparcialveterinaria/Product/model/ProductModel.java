7178d60af31ae1b171be21173c69f5b3f8c3b3f7
GerardoMejia1107 <00104923@uca.edu.sv>
2026-05-25 18:57:37 -0600

    fix: :hammer: Adding column names to model fields

--- src/main/java/com/uca/pncsegundoparcialveterinaria/Product/model/ProductModel.java
+++ src/main/java/com/uca/pncsegundoparcialveterinaria/Product/model/ProductModel.java
@@ -14,13 +14,23 @@ public class ProductModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "description")
     private String description;
+    @Column(name = "category")
+    @Enumerated(EnumType.STRING)
     private Category category;
+    @Column(name = "price")
     private BigDecimal price;
+    @Column(name = "stock")
     private Integer stock;
+    @Column(name = "available")
     private Boolean available;
+    @Column(name = "requires_prescription")
     private Boolean requiresPrescription;
+    @Column(name = "expiration_date")
     private LocalDateTime expirationDate;
+    @Column(name = "supplier")
     private String supplier;
 }
