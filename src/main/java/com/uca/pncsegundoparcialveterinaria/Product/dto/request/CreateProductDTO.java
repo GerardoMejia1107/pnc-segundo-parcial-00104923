6d43c19ef916fcdf6004f0f901d9ddd2e25a0143
GerardoMejia1107 <00104923@uca.edu.sv>
2026-05-25 19:11:06 -0600

    feat: :sparkles: Adding create request for product dto

---
+++ src/main/java/com/uca/pncsegundoparcialveterinaria/Product/dto/request/CreateProductDTO.java
@@ -0,0 +1,39 @@
+package com.uca.pncsegundoparcialveterinaria.Product.dto.request;
+
+import com.uca.pncsegundoparcialveterinaria.Product.utils.Category;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class CreateProductDTO {
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    private String description;
+
+    @NotNull(message = "Category is required")
+    private Category category;
+    @NotNull(message = "Price is required")
+    @Min(value = 1)
+    private BigDecimal price;
+
+    @NotNull(message = "Stock value is required")
+    @Min(value = 0)
+    private Integer stock;
+
+
+    private Boolean available;
+
+    private Boolean requiresPrescription;
+
+    @NotNull(message = "Expiration date is required")
+    private LocalDateTime expirationDate;
+    @NotBlank(message = "Supplier is required")
+    private String supplier;
+
+}
---
+++ src/main/java/com/uca/pncsegundoparcialveterinaria/Product/dto/response/ResponseProductDTO.java
@@ -0,0 +1,4 @@
+package com.uca.pncsegundoparcialveterinaria.Product.dto.response;
+
+public class ResponseProductDTO {
+}
