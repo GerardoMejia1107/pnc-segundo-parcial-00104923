1932f3be21a8d220588ff182cfb544790bab7bc7
GerardoMejia1107 <00104923@uca.edu.sv>
2026-05-25 19:14:37 -0600

    feat: :sparkles: Adding response dto for product

--- src/main/java/com/uca/pncsegundoparcialveterinaria/Product/dto/response/ResponseProductDTO.java
+++ src/main/java/com/uca/pncsegundoparcialveterinaria/Product/dto/response/ResponseProductDTO.java
@@ -1,4 +1,19 @@
 package com.uca.pncsegundoparcialveterinaria.Product.dto.response;
 
+import com.uca.pncsegundoparcialveterinaria.Product.utils.Category;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class ResponseProductDTO {
+    private String name;
+    private String description;
+    private Category category;
+    private BigDecimal price;
+    private Integer stock;
+    private Boolean available;
+    private Boolean requiresPrescription;
+    private LocalDateTime expirationDate;
+    private String supplier;
 }
